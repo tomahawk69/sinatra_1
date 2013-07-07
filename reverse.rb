b4f7b73b73cc283be32c9e827470ff5f504b9978
tomahawk69 <yur.ovchynnikov@gmail.com>
2013-07-07 14:37:12 +0300

    Added errors catching

--- reverse.rb
+++ reverse.rb
@@ -1,10 +1,27 @@
 require 'sinatra'
 
+class ParamError < StandardError; end
+
 
 get '/' do
 	erb :home
 end
 
 post '/reverse' do
+	if not params[:text] or params[:text].empty? then
+		raise ParamError, "Parameter 'text' is missing or empty" 
+
+	end
 	erb :reverse
 end
+
+# errors
+not_found do
+	erb :'404'
+end
+
+error do
+	@error = request.env['sinatra_error']
+   	erb :'param_error'
+end
+
